99b2d67490d1547085aaff4e718646b1e84cf199
MikeyG16 <michael_glossop@yahoo.co.uk>
2026-09-02 19:14:30 +0100

    Started Sprint 3

--- Crawler.js
+++ Crawler.js
@@ -44,7 +44,7 @@ class Crawler {
 
         // TEST LIMIT
         // Change "< 5" to whatever you like whilst testing. && this.pages.length < 10
-        while (this.queue.length > 0 && this.pages.length < 50) {
+        while (this.queue.length > 0) {
 
             const current = this.queue.shift();
 
